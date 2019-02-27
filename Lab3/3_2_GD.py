a7a95994d7a430e1154d6232ad99fceca8879e94
NagaTheDon <nagaarjun09@gmail.com>
2019-02-27 14:49:20 +0000

    Clean ups

--- Lab3/3_2_GD.py
+++ Lab3/3_2_GD.py
@@ -7,14 +7,12 @@ import math
 M = 50
 theta_true = torch.Tensor([[0.5], [2]])
 
-
+#X and Y
 X = 10 * torch.rand(M, 2) - 5
 X[:, 1] = 1.0
-print("X shape: ", X.shape)
 
 y = torch.mm(X, theta_true) + 0.3 * torch.randn(M, 1)
 
-## hypothesis computes $h_theta$
 def hypothesis(theta, X):
     h_theta = torch.mm(X, theta)
     return h_theta
@@ -28,8 +26,7 @@ def grad_cost_func(theta, X, y):
     grad = (1/M)*sum_term
     return grad
 
-    
-
+#Cost function
 def cost_func(theta, X, y):
 
     sum_term = ((hypothesis(theta, X) - y)**2).sum(0)
@@ -37,7 +34,7 @@ def cost_func(theta, X, y):
     return cost
 
 
-## The weight update computed using the ADAM optimisation algorithm
+# The weight update computed using the ADAM optimisation algorithm
 def weightupdate_adam(count, X, y):
 	#lo refers to local variable in this function
     der_w = grad_cost_func(theta_0, X, y)
@@ -51,7 +48,7 @@ def weightupdate_adam(count, X, y):
     return V_dw_lo, S_dw_lo, theta
 
 
-## The weight update computed using SGD + momentum
+# The weight update computed using SGD + momentum
 def weightupdate_sgd_momentum(count, X, y):
 	#lo refers to local 
 	der_w = grad_cost_func(theta_1, X, y)
@@ -61,26 +58,26 @@ def weightupdate_sgd_momentum(count, X, y):
 	return V_t_lo, theta
 
 
-## The weight updated computed using SGD
+# The weight updated computed using SGD
 def weigthupdate_sgd(count, X, y):
 	der_w = grad_cost_func(theta_2, X, y)
 	theta = theta_2 - (alpha*der_w)
 	return theta
 
 
+#Computations
 
-
-
+# Constants 
 N = 200
 beta_1 = 0.9
 beta_2 = 0.999
 alpha = 0.01
 batch_size = 10
 
-# Need to ask people in lab: for 200 iterations -> N+1 isn't it?
 #Adam
 theta_ADAM = torch.zeros((2,1,N))
 
+#Initial values
 V_dw = 0
 S_dw = 0
 theta_0 = torch.Tensor([[2],[4]]) #initialise
@@ -100,6 +97,7 @@ for i in range(1,N):
 # weightupdate_sgd_momentum
 theta_sgd_m = torch.zeros((2,1,N))
 
+#Initial values
 V_t = 0
 theta_1 = torch.Tensor([[2],[4]])
 
@@ -114,9 +112,12 @@ for i in range(1,N):
 
 	theta_sgd_m[:,:,i] = theta_1
 
+
+
 # weightupdate_sgd
 theta_sgd = torch.zeros((2,1,N))
 
+#Initial values
 theta_2 = torch.Tensor([[2],[4]])
 
 theta_sgd[:,:,0] = theta_2
@@ -146,15 +147,15 @@ for zero_ind in range(100):
 
 xc,yc = np.meshgrid(theta_0_vals, theta_1_vals)
 contours = plt.contour(xc, yc, J, 20)
+
+#Plot the points 
 adam_point = plt.scatter(theta_ADAM[0,:,:], theta_ADAM[1,:,:], marker='o', s=20, c='red')
 sgd_m_point = plt.scatter(theta_sgd_m[0,:,:], theta_sgd_m[1,:,:], marker='o', s=20, c='cyan')
 sgd = plt.scatter(theta_sgd[0,:,:], theta_sgd[1,:,:], marker='o', s=20, c='green')
 
+
 plt.legend((adam_point, sgd_m_point, sgd),
            ('ADAM', 'SGD with Momentum', 'SGD'),loc='lower right',
             fontsize=8)
-
 plt.show()
 
-
-# Now plot the output of SGD, momentum and Adam all on the same plot for comparison
